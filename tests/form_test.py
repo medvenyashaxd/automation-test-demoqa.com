b69b325e8293343f213d59adc734bb40d2bf695f
medvenyashaXD <xmedvenyashax@gmail.com>
2022-09-07 20:26:16 +0300

    added allure in form_page, form_test.py

--- tests/form_test.py
+++ tests/form_test.py
@@ -1,11 +1,16 @@
 import time
+import allure
 
 from pages.form_page import PracticeFormPage
 
 
+@allure.suite('Test practice form')
 class TestPracticeForm:
+
+    @allure.feature('Test student registration form')
     class TestStudentRegistrationForm:
 
+        @allure.title('Check practice form')
         def test_practice_form(self, driver):
             practice_form_page = PracticeFormPage(driver, 'https://demoqa.com/automation-practice-form')
             practice_form_page.open()
@@ -13,4 +18,4 @@ class TestPracticeForm:
             input_info = practice_form_page.fill_fields()
             output_info = practice_form_page.check_submitting_form()
             assert [input_info.first_name + ' ' + input_info.last_name, input_info.email] == [output_info[0],
-                    output_info[1]], 'table form is filled incorrectly'
+                   output_info[1]], 'table form is filled incorrectly'
